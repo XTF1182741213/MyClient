49528b3b929fdb5937907b7cf50e5ade8f0358fc
XTF1182741213 <1182741213@qq.com>
2017-08-08 09:12:15 +0800

    first commit

---
+++ src/com/example/myclient/Personal.java
@@ -0,0 +1,13 @@
+package com.example.myclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Personal extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personal);
+    }
+}
